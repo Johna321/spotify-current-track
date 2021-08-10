f6a7751643262adec4548ba875f43c05702eb742
john aitken <johna321123@gmail.com>
2021-08-09 22:21:59 -0400

    initial commit, basic script to fetch current track data from spotify api

---
+++ spotify-current-playing.py
@@ -0,0 +1,22 @@
+#!/bin/python3
+
+import requests
+
+SPOTIFY_CURRENT_PLAYING_URL = "https://api.spotify.com/v1/me/player/currently-playing"
+SPOTIFY_BEARER_TOKEN = ""
+
+def fetchSpotifyData(spotifyUrl, bearerToken):
+    res = requests.get(SPOTIFY_CURRENT_PLAYING_URL, headers={
+        "Authorization": f"Bearer {SPOTIFY_BEARER_TOKEN}"
+    })
+    res = res.json()
+    trackName = res['item']['name']
+    artists = res['item']['artists']
+    artistName = ", ".join([artist['name'] for artist in artists])
+    print(f"{trackName} - {artistName}")
+
+def main():
+    fetchSpotifyData(SPOTIFY_CURRENT_PLAYING_URL, SPOTIFY_BEARER_TOKEN)
+
+if __name__ == '__main__':
+    main()
