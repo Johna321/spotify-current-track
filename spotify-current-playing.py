f3185a571de57ba949c850f39b8d999611834f73
john aitken <johna321123@gmail.com>
2021-08-10 09:58:04 -0400

    fixed error handling

--- spotify-current-playing.py
+++ spotify-current-playing.py
@@ -6,11 +6,11 @@ SPOTIFY_CURRENT_PLAYING_URL = "https://api.spotify.com/v1/me/player/currently-pl
 SPOTIFY_BEARER_TOKEN = ""
 
 def fetchSpotifyData(spotifyUrl, bearerToken):
-    res = requests.get(SPOTIFY_CURRENT_PLAYING_URL, headers={
-        "Authorization": f"Bearer {SPOTIFY_BEARER_TOKEN}"
-    })
-    res = res.json()
     try:
+        res = requests.get(SPOTIFY_CURRENT_PLAYING_URL, headers={
+            "Authorization": f"Bearer {SPOTIFY_BEARER_TOKEN}"
+        })
+        res = res.json()
         trackName = res['item']['name']
         artists = res['item']['artists']
         artistName = ", ".join([artist['name'] for artist in artists])
